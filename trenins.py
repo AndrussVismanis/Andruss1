5a39ee0c1030c5571aa50269e58dd8f615c721e9
AndrussVismanis <149487385+AndrussVismanis@users.noreply.github.com>
2024-05-21 13:36:09 +0300

    Create trenins.py

---
+++ trenins.py
@@ -0,0 +1,59 @@
+def rezultats(sk1, sk2):
+    if sk1<6 and sk2<6:
+        rez = sk1*sk2
+       else:
+          rez= sk1+sk2
+
+        return rez
+
+for skaitlis in range(1,11, 2):     #range - funkcija, kas skaita skaitļus
+    for otrs in range(1,11, 2)
+       print("pirmais skaitlis:", skaitlis, " otrais skaitlis:","rezultāts", rezultats(skaitlis otrs))
+    
+
+
+    def zvaigznites(skaits):
+        for rindasNr in range(1, skaits+1)
+           for zvaigzne in range(rindasNr)
+        print("*", end="")
+    print("")
+
+zvaigznites(7)
+
+skaitlis1 = 7
+skaitlis2 = 3
+
+print("pirmais skaitlis:", skaitlis1)
+print("otrais skaitlis:", skaitlis2)
+print("rezultāts", rezultats(skaitlis1,skaitlis2))
+
+pirmais = "6"
+
+print(vards)
+
+vards2 = "NĒ"
+
+print(pirmais + vards2)
+
+
+zvaigznites1(7)
+
+
+saraksts1 = [1, 7, 5, 9, 35, 2]
+saraksts2 = [4, 2, 2, 39, 6, 4]
+
+for skaititajs in range(len(saraksts1)):
+    print("skaititajs:", skaititajs ,"pirmais skaitlis", saraksts2[skaititajs], "rezultāts: ", rezultats(saraksts1[skaititajs], saraksts2[skaititajs]))
+
+skaitlu_pari = [[2,5], [4,7], [3,4], [7,9]]
+ print("---------------")
+
+for i in range(len(skaitlu_pari)):
+   print("skaititajs", i ,"pirmais skaitlis", skaitlu_pari[i][0],"otrais skaitlis:", skaitlu_pari[i][1], "rezultāts", rezultats(saraksts1[skaititajs], saraksts2[skaititajs]))
+
+    print("---------------")
+
+for elements in skaitlu_pari:
+    print("pirmais skaitlis:", elements[0],"otrais skaitlis:" elements[1], "rezultāts: ", rezultats (elements[0], elements[1]))
+
+
